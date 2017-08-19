da70931ae8b0fef580e250a92825eb0cc3ca554d
Noel Bourke <undergroundquizscene@gmail.com>
2017-08-19 02:08:53 +0100

    Use scale with note numbers
    Rather than hardcoding frequencies, I’ve created an EqualTemperament
    object, which maps note numbers to their equal temperament frequencies,
    and then scale objects which map relative notes (e.g. 0 to 7 for a
    major scale) to 12-tone note numbers.
    
    I’m not quite sure what the object structure I want is yet. I think
    passing the scale to the sequencer makes sense, as it’ll probably
    determine the notes that are available on some kind of input grid or
    something.
    
    I think the next thing to figure out is a basic input system, along
    with a stop/start option (maybe the stop/start first actually!). It’d
    also be good to allow playing multiple notes at a time, and possibly
    encapsulating the notes interaction – the sequencer could call
    something like getNotesUpTo() to get the notes to be scheduled. I’ll
    have to think about whether tempo information should stay in the
    sequencer or the note-encapsulating object.

--- sequencer.js
+++ sequencer.js
@@ -1,47 +1,48 @@
 let Sequencer = {
-    init: function() {
+    init: function(scale) {
        this.output = this.audioContext.createGain();
        this.output.gain.value = 0.25;
        this.output.connect(this.audioContext.destination);
+       this.scale = scale;
     },
     audioContext: new AudioContext(),
     lookahead: 0.1,
     scheduleIntervalMs: 25,
     tempo: 144,
-    nextNoteTime: 1,
+    nextNoteTime: 0.5,
     beatNumber: 0,
     notes: [
         [
             {
-                length: 1,
-                frequency: 800,
-            },
-        ],
-        [
-            {
-                length: 1,
-                frequency: 200,
-            },
-        ],
-        [
-            {
-                length: 4,
-                frequency: 700,
+                length: 2,
+                note: 0,
             },
         ],
         [],
+        [
+            {
+                length: 2,
+                note: 1,
+            },
+        ],
         [],
+        [
+            {
+                length: 2,
+                note: 2,
+            },
+        ],
         [],
         [
             {
                 length: 1,
-                frequency: 400,
+                note: 3,
             },
         ],
         [
             {
                 length: 1,
-                frequency: 320,
+                note: 4,
             },
         ],
     ],
@@ -50,13 +51,12 @@ let Sequencer = {
             for (let thisNote of this.notes[this.beatNumber]) {
                 let oscillator = this.audioContext.createOscillator();
                 oscillator.type = 'sine';
-                oscillator.frequency.value = thisNote.frequency;
+                oscillator.frequency.value = this.scale.frequencyOf(thisNote.note);
+                console.log('Note frequency: ' + this.scale.frequencyOf(thisNote.note));
 
                 oscillator.connect(this.output);
                 oscillator.start(this.nextNoteTime);
-                console.log('Note to start at ' + this.nextNoteTime);
                 oscillator.stop(this.nextNoteTime + this.noteLength(thisNote.length));
-                console.log('Note to finish at ' + this.noteLength(thisNote.length));
             }
             this.nextNote();
         }
@@ -72,7 +72,33 @@ let Sequencer = {
     },
 };
 
-Sequencer.init();
+let EqualTemperament = {
+    referenceFreq: 440,
+    frequencyOf: function(noteNumber) {
+        return this.referenceFreq * Math.pow(2, noteNumber / 12);
+    }
+};
+
+let OctaveScale = {
+    frequencyOf: function(noteNumber) {
+        let divide = Math.trunc(noteNumber / this.notes.length);
+        let mod = noteNumber % this.notes.length;
+
+        let extendedNoteNumber = divide * 12 + this.notes[mod];
+
+        return this.tuningSystem.frequencyOf(extendedNoteNumber);
+    },
+    createScale: function({tuningSystem=EqualTemperament, scaleNotes}) {
+        let newScale = Object.create(this);
+        newScale.tuningSystem = tuningSystem;
+        newScale.notes = scaleNotes;
+        return newScale;
+    }
+};
+
+let MajorPentatonicScale = OctaveScale.createScale({scaleNotes: [0, 2, 4, 7, 9]});
+
+Sequencer.init(MajorPentatonicScale);
 
 let boundSchedule = Sequencer.scheduleNotes.bind(Sequencer);
 
