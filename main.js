0f76fd25d507f04b180f4c739759eb0e7b31945f
Noel Bourke <undergroundquizscene@gmail.com>
2018-03-01 00:05:49 +0000

    Do basic electron setup

---
+++ main.js
@@ -0,0 +1,15 @@
+const {app, BrowserWindow} = require('electron');
+const url = require('url');
+const path = require('path');
+
+function createWindow() {
+    win = new BrowserWindow({width: 800, height: 600});
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'demo.html'),
+        protocol: 'file:',
+        slashes: true,
+    }));
+}
+
+app.on('ready', createWindow);
